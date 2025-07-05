6b2ab6cef36962d30b468bc4b12c410d6fc80c2c
Ayu456sh <154459231+Ayu456sh@users.noreply.github.com>
2025-07-06 02:04:20 +0530

    Time: 4 ms (100.00%) | Memory: 43.9 MB (88.51%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0 || x%10==0 && x!=0){
+            return false;
+        }
+        int digit =0;
+
+        while(x>digit){
+            digit=digit*10+ x%10;
+            x=x/10;
+        }
+        return x==digit || x==digit/10;
+    }
+}
