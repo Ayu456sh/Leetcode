c49d2f07b9d9f8d61ceb45ec9f7e0a7694ae229e
Ayush Kakkar <154459231+Ayu456sh@users.noreply.github.com>
2025-07-14 01:02:16 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (52.47%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,16 +1,17 @@
 class Solution {
     public int numberOfSteps(int num) {
-        
-        int even = num/2;
-        int odd = num-1;
+        return helper(num , 0);
+    }
+    private int helper(int num , int steps){
         if(num==0){
-            return 0;
+            return steps;
         }
+
         if(num%2==0){
-            return 1+numberOfSteps(even);
+            return helper(num/2 , steps+1);
         }
         else{
-            return 1+numberOfSteps(odd);
+            return helper(num-1 , steps+1);
         }
     }
 }
