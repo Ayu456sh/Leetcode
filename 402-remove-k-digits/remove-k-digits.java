5265b5126ed6bfb19bb9d248164db37a37b701b0
Ayush Kakkar <154459231+Ayu456sh@users.noreply.github.com>
2025-08-17 18:32:43 +0530

    Time: 26 ms (35.77%) | Memory: 45.8 MB (30.65%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.java
+++ 402-remove-k-digits/remove-k-digits.java
@@ -10,21 +10,20 @@ class Solution {
             st.push(num.charAt(i));
         }
         while(k>0){
-            st.pop();
-            k--;
+            st.pop(); // [1,2,3,4,5] and k=2  then till end we will pop the end 2 elements.
+            k--; // this loop is for if the stack haven't remove a element from stack till last.
         }
         StringBuilder res = new StringBuilder();
         if(st.isEmpty())return "0";
         while(!st.isEmpty()){
-           res.append(st.pop());
+            res.append(st.pop());
         }
         res.reverse();
         while(res.length()!=0 && res.charAt(0)=='0'){
             res.deleteCharAt(0);
         }
-        if(res.isEmpty()){
-            return "0";
-        }
+        if(res.isEmpty())return "0";
+
         return res.toString();
     }
 }
