317fb4433acacedcc7ddaa8d218fa6aa65198374
Ayush Kakkar <154459231+Ayu456sh@users.noreply.github.com>
2025-08-19 21:13:40 +0530

    Time: 6 ms (7.84%) | Memory: 42.5 MB (26.85%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,39 +1,37 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
+        List<String> ans = new ArrayList<>();
         if(digits.isEmpty()){
-            ArrayList<String> list = new ArrayList<>();
-            return list;
+            return ans;
         }
         return helper("",digits);
     }
-    private List<String> helper(String p , String digits){
-        if(digits.isEmpty()){
-            ArrayList<String> res = new ArrayList<>();
-            res.add(p);
-            return res;
+    private List<String> helper(String p , String up){
+        if(up.isEmpty()){
+            List<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
         }
-        int num = digits.charAt(0)-'0';
-        
-        ArrayList<String> list = new ArrayList<>();
+        int num = up.charAt(0)-'0';
+        List<String> list = new ArrayList<>();
 
         String letter = getLetters(num);
-        for(int i = 0; i<letter.length(); i++){
-            
+        for(int i=0; i<letter.length(); i++){
             char ch = letter.charAt(i);
-            list.addAll(helper(p+ch, digits.substring(1)));
+            list.addAll(helper(p+ch,up.substring(1)));
         }
         return list;
     }
     private String getLetters(int num){
         switch(num){
-            case 2 : return "abc";
-            case 3 : return "def";
-            case 4 : return "ghi";
-            case 5 : return "jkl";
-            case 6 : return "mno";
-            case 7 : return "pqrs";
-            case 8 : return "tuv";
-            case 9 : return "wxyz";
+            case 2: return "abc";
+            case 3: return "def";
+            case 4: return "ghi";
+            case 5: return "jkl";
+            case 6: return "mno";
+            case 7: return "pqrs";
+            case 8: return "tuv";
+            case 9: return "wxyz";
             default: return "";
         }
     }
