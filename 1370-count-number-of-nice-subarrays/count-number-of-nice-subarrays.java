92058a7a48a30c35d66fa37e712c0d35e3c619d4
Ayush Kakkar <154459231+Ayu456sh@users.noreply.github.com>
2025-10-19 00:10:23 +0530

    Time: 13 ms (60.96%) | Memory: 55.5 MB (27.15%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -2,20 +2,23 @@ class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
         return helper(nums,k)-helper(nums,k-1);
     }
-    public int helper(int[] nums , int k){
+    private int helper(int[] nums , int k){
         int start=0;
         int end=0;
-        int sum=0;
+        int oddCount=0;
         int count=0;
 
         while(end<nums.length){
-            if(nums[end]%2==1)sum++;
-
-            while(sum>k){
-                if(nums[start]%2==1)sum--;
+            if(nums[end]%2==1){
+                oddCount++;
+            }
+            while(oddCount>k){
+                if(nums[start]%2==1){
+                    oddCount--;
+                }
                 start++;
             }
-            count+=(end-start+1);
+            count+=end-start+1;
             end++;
         }
         return count;
