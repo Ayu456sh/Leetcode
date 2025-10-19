9452b5b79a31499d11ce7ac1869a8dddff0901ce
Ayush Kakkar <154459231+Ayu456sh@users.noreply.github.com>
2025-10-19 23:55:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (99.48%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,13 +1,12 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> outer = new ArrayList<>();
-
         outer.add(new ArrayList<>());
 
         for(int num : nums){
             int n = outer.size();
             for(int i=0; i<n; i++){
-                ArrayList<Integer> internal = new ArrayList<>(outer.get(i));
+                List<Integer> internal = new ArrayList<>(outer.get(i));
                 internal.add(num);
                 outer.add(internal);
             }
