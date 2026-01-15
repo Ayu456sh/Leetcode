fb7a9a55626c7de026068307f3424081f6974640
Ayush Kakkar <154459231+Ayu456sh@users.noreply.github.com>
2026-01-15 12:47:57 +0530

    Time: 4 ms (99.78%) | Memory: 46 MB (38.99%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -3,11 +3,11 @@ class Solution {
         if(x<0 || x%10==0 && x!=0){
             return false;
         }
-        int org =0;
+        int org=0;
         while(x>org){
             org=org*10+x%10;
             x=x/10;
         }
-        return org==x || x==org/10;
+        return x==org || x== org/10;
     }
 }
